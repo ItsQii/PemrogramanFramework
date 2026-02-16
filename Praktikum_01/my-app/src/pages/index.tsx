ad45ccef692fe5c436496eea58d26cb7ef88c147
Ripqii <arsya7926@gmail.com>
2026-02-16 09:29:25 +0700

    Tugas 2

--- Praktikum_01/my-app/src/pages/index.tsx
+++ Praktikum_01/my-app/src/pages/index.tsx
@@ -1,9 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -16,7 +15,14 @@ export default function Home() {
         <h1>Praktikum Next.js Pages Router</h1>
         <br />
         <p>Mahasiswa D4 Pengembangan Web</p>
+
+        <Link
+          href="/about"
+          style={{ color: "blue", textDecoration: "underline" }}
+        >
+          Tentang Saya
+        </Link>
       </div>
     </>
-  )
+  );
 }
