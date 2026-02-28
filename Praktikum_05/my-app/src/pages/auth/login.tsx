a2d0584a5f44691156a968c70406c59ed309b65e
Ripqii <arsya7926@gmail.com>
2026-02-28 11:25:04 +0700

    LANGKAH 5

--- Praktikum_05/my-app/src/pages/auth/login.tsx
+++ Praktikum_05/my-app/src/pages/auth/login.tsx
@@ -1,27 +1,10 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import styles from "./login.module.css";
+import TampilanLogin from "../../views/auth/login";
 
 const halamanLogin = () => {
-  const router = useRouter();
-
-  const handleLogin = () => {
-    sessionStorage.setItem("login", "true");
-    router.push("/produk");
-  };
-
   return (
-    <div className={styles.login}>
-      <h1>Halaman Login</h1>
-
-      <button onClick={handleLogin}>
-        Login
-      </button>
-
-      <br /><br />
-
-      <Link href="/auth/register">Ke Halaman Register</Link>
-    </div>
+    <>
+      <TampilanLogin />
+    </>
   );
 };
 
