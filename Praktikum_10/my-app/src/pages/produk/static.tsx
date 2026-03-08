2e12b05d96185f6442eeb4d86d61f57888c525d8
Ripqii <arsya7926@gmail.com>
2026-03-08 15:07:16 +0700

    Langkah 1 Setup Halaman Static

---
+++ Praktikum_10/my-app/src/pages/produk/static.tsx
@@ -0,0 +1,26 @@
+import TampilanProduk from "../../views/produk/MainSection";
+import { ProductType } from "../types/Product.type";
+
+const halamanProdukStatic = (props: { products: ProductType[] }) => {
+  const { products } = props;
+  return (
+    <div>
+      <h1>Halaman Produk Static</h1>
+      <TampilanProduk products={products} />
+    </div>
+  );
+};
+
+export default halamanProdukStatic;
+
+export async function getStaticProps() {
+  // Mengambil data dari API saat proses build
+  const res = await fetch("http://localhost:3000/api/produk");
+  const response = await res.json();
+
+  return {
+    props: {
+      products: response.data || [],
+    },
+  };
+}
