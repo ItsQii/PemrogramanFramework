4ed823c60d8fbc7d3db454746d0071439e695379
Ripqii <arsya7926@gmail.com>
2026-04-01 18:53:04 +0700

    Modifikasi index.tsx pada folder components/layouts/navbar

--- Praktikum_14/my-app/src/components/layouts/navbar/index.tsx
+++ Praktikum_14/my-app/src/components/layouts/navbar/index.tsx
@@ -2,16 +2,36 @@ import styles from "./navbar.module.scss";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 const Navbar = () => {
-  const { data } = useSession();
+  const { data }: any = useSession();
 
   return (
     <div className={styles.navbar}>
-      <div className="big">Navbar</div>
-      {data ? (
-        <button onClick={() => signOut()}>Sign Out</button>
-      ) : (
-        <button onClick={() => signIn()}>Sign In</button>
-      )}
+      <div className={styles.navbar__brand}>
+        MyApp
+      </div>
+
+      <div className={styles.navbar__right}>
+        {data ? (
+          <>
+            <div className={styles.navbar__user}>
+              Welcome, {data.user?.fullname}
+            </div>
+            <button
+              className={`${styles.navbar__button} ${styles["navbar__button--danger"]}`}
+              onClick={() => signOut()}
+            >
+              Sign Out
+            </button>
+          </>
+        ) : (
+          <button
+            className={`${styles.navbar__button} ${styles["navbar__button--primary"]}`}
+            onClick={() => signIn()}
+          >
+            Sign In
+          </button>
+        )}
+      </div>
     </div>
   );
 };
