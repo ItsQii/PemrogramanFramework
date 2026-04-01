055282959d7e965da2d310e427b86428623c99c5
Ripqii <arsya7926@gmail.com>
2026-04-01 19:45:23 +0700

    Buat Middleware Authorization

--- Praktikum_14/my-app/src/middleware.ts
+++ Praktikum_14/my-app/src/middleware.ts
@@ -1,19 +1,31 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-export function middleware(request: NextRequest) {
-  const isLogin = true;
-  
+export async function middleware(request: NextRequest) {
+  // 1. Mengambil token dari sesi NextAuth
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  const isLogin = !!token;
+
+  if (request.nextUrl.pathname.startsWith("/profile")) {
+    if (!isLogin) {
+      // Jika belum login atau sudah logout, redirect ke home ("/")
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+
   if (isLogin) {
     return NextResponse.next();
   } else {
+    // Jika belum login dan mengakses /produk atau /about, lempar ke halaman login
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
-
-  // return NextResponse.redirect(new URL("/", request.url));
-  // return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/produk", "/about"],
+  matcher: ["/produk", "/about", "/profile"],
 };
