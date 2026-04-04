32bedc02505299d8aab854a9cb555df67062e8ca
Ripqii <arsya7926@gmail.com>
2026-04-04 12:33:41 +0700

    Terapkan dynamic import pada minimal 1 komponen

--- Praktikum_18/my-app/src/pages/_app.tsx
+++ Praktikum_18/my-app/src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '@/components/layouts/navbar';
-import AppShell from '@/components/layouts/Appshell';
 import { SessionProvider } from "next-auth/react";
 import Script from 'next/script';
+import dynamic from 'next/dynamic';
+
+const AppShell = dynamic(() => import('@/components/layouts/Appshell'), {
+  ssr: false,
+});
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      {/* POIN 3: Google Analytics */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-123456789"
         strategy="afterInteractive"
@@ -17,7 +22,6 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
           gtag('config', 'G-123456789');
         `}
       </Script>
