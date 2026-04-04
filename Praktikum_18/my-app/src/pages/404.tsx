0bf03c401e8e5877c0b7da447ba65eddc63b2583
Ripqii <arsya7926@gmail.com>
2026-04-04 12:30:35 +0700

    PRAKTIKUM 1 – Image Optimization

--- Praktikum_18/my-app/src/pages/404.tsx
+++ Praktikum_18/my-app/src/pages/404.tsx
@@ -1,26 +1,21 @@
-import Link from "next/link";
-import styles from '@/styles/404.module.scss';
+import Image from "next/image";
+import styles from "@/styles/404.module.scss";
 
 const Custom404 = () => {
   return (
     <div className={styles.error}>
-      <img 
-        src="/page-not-found.png" 
-        alt="404" 
-        className={styles.error__image} 
+      {/* <img src="/page-not-found.png" alt="404" className={styles.error__image} /> */}
+      
+      <Image
+        src="/page-not-found.png"
+        alt="404"
+        width={400}
+        height={200}
+        className={styles.error__image}
       />
 
-      <h1 className={styles.error__title}>
-        404 - Halaman Tidak Ditemukan
-      </h1>
-
-      <p className={styles.error__desc}>
-        Maaf, halaman yang Anda cari tidak ditemukan.
-      </p>
-
-      <Link href="/" className={styles.error__button}>
-        Kembali ke Home
-      </Link>
+      <h1>404 - Halaman Tidak Ditemukan</h1>
+      <p>Maaf, halaman yang Anda cari tidak ada.</p>
     </div>
   );
 };
