082c757527db0ac68705cc60f57dbb0b5370699e
Ripqii <arsya7926@gmail.com>
2026-04-04 12:33:03 +0700

    TUGAS

--- Praktikum_18/my-app/src/pages/_app.tsx
+++ Praktikum_18/my-app/src/pages/_app.tsx
@@ -3,10 +3,25 @@ import type { AppProps } from 'next/app'
 import Navbar from '@/components/layouts/navbar';
 import AppShell from '@/components/layouts/Appshell';
 import { SessionProvider } from "next-auth/react";
+import Script from 'next/script';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-123456789"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-123456789');
+        `}
+      </Script>
+
       <AppShell>
         <Component {...pageProps} />
       </AppShell>
