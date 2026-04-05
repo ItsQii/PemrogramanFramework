5704cb8b98f0e4864385643db0fa14047f4924d6
Ripqii <arsya7926@gmail.com>
2026-04-05 17:53:43 +0700

    TUGAS 1

--- Praktikum_19/Unit-Testing/src/components/layouts/Appshell/index.tsx
+++ Praktikum_19/Unit-Testing/src/components/layouts/Appshell/index.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import Navbar from "../navbar";
+import Footer from "@/components/layouts/Footer";
 import { Roboto } from "next/font/google";
 
 const disableNavbar = ["/auth/login", "/auth/register", "/404", "/dashboard"];
@@ -18,9 +19,17 @@ const AppShell = (props: AppShellProps) => {
   const { pathname } = useRouter();
 
   return (
-    <main className={roboto.className}>
+    <main 
+      className={roboto.className} 
+      style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}
+    >
       {!disableNavbar.includes(pathname) && <Navbar />}
-      {children}
+      
+      <div style={{ flex: 1 }}>
+        {children}
+      </div>
+
+      {!disableNavbar.includes(pathname) && <Footer />}
     </main>
   );
 };
--- Praktikum_19/Unit-Testing/src/components/layouts/Footer.tsx
+++ Praktikum_19/Unit-Testing/src/components/layouts/Footer.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function Footer(){
+import styles from '@/components/layouts/navbar/navbar.module.scss' 
+
+export default function Footer() {
   return (
-    <footer className="footer" style={{textAlign: 'center', padding: '1rem 0', borderTop: '1px solid #eaeaea'}}>
-      © 2026 Praktikum Framework
+    <footer className={styles.footer}>
+      <div className={styles.footer__content}>
+        <h3 className={styles.footer__title}>Framework Based Programming</h3>
+        <div className={styles.footer__divider}></div>
+        <h3>With Great Power Comes Great Responsibility.</h3>
+      </div>
     </footer>
   )
 }
